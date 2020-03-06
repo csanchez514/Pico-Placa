48512e1824c3dfdd1e0f72043b15701d2defa749
csanchez514 <chris.beto.18@gmail.com>
2020-03-06 18:47:15 -0500

    reparación proyecto v.1

---
+++ app/Http/Controllers/PlacaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Placa;
+use Illuminate\Http\Request;
+
+class PlacaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+}
---
+++ app/Placa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+
+class Placa 
+{
+    private $placa;
+    private $fecha;
+    private $hora;
+
+    public function VerificarFecha ($fechaentrada)
+    {
+
+    }
+    public function VerificarHora ($horaentrada)
+    {
+        
+    }
+    
+}
